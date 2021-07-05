72da086bec4aa548285835e98a2dd029c1198601
savannahvaith <sav.vaithilingam@gmail.com>
2021-07-05 12:41:20 +0100

    SV: Added Simple explanation of Controller

---
+++ src/main/java/com/qa/springcars/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.qa.springcars.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Allows for external access to our application via HTTPRequests
+public class TestController {
+	
+	// Allows our program to accept REQUESTS and send RESPONSES!
+	
+	@GetMapping("/test") // @TypeOfRequest("location")
+	public String thisMethod() {
+		return "Hi"; 
+	}
+
+}
