8f9cc5f3809809b6ba41fc49f3f8fcf4ce0cc13a
savannahvaith <sav.vaithilingam@gmail.com>
2021-07-08 13:02:22 +0100

    SV: Updated Car Controller to use Car ServiceDB

--- src/main/java/com/qa/springcars/controller/CarController.java
+++ src/main/java/com/qa/springcars/controller/CarController.java
@@ -7,29 +7,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.springcars.domain.Car;
-import com.qa.springcars.service.CarServiceList;
+import com.qa.springcars.service.CarServiceDB;
 
 @RestController
+//@RequestMapping("/Car")
 public class CarController {
 	
 	// Mapping URLS to METHODS
 	
-	private CarServiceList service; 
+	private CarServiceDB service; 
 	
 	// PULL DOWN THE OBJECT FROM THE BEANBAG
 	
-	@Autowired
-	public CarController(CarServiceList service) {
+	@Autowired // <- Optional
+	public CarController(CarServiceDB service) {
 		this.service = service;
 	}
 	
+	@GetMapping("/test2") // @TypeOfRequest("location")
+	public String thisMethod() {
+		return "Hello from CAR CONTROLLER"; 
+	}
 	
 	// CRUD functionality
 	
@@ -49,25 +55,25 @@ public class CarController {
 	}
 	
 	// UPDATE - PUT (REPLACE) / PATCH (UPDATE an ELEMENT) 
-	@PatchMapping("/updateCar/{index}")
-	public ResponseEntity<Car> updateCar(@PathVariable int index, @RequestBody Car newCar) {
-		
-		// replace the WHOLE object in a specific INDEX
-		this.service.updateCar(index, newCar);
-		
-		return new ResponseEntity<Car>(newCar, HttpStatus.ACCEPTED);
-		
-		// OR Set each individual variable: 
-//		Car oldCar = this.vehicles.get(index); 
-//		oldCar.setMake(newCar.getMake());
-//		oldCar.setModel(newCar.getModel());
-//		oldCar.setEngine(newCar.getEngine());
-//		oldCar.setSpoiler(newCar.isSpoiler());
-	}
+//	@PutMapping("/updateCar/{index}")
+//	public ResponseEntity<Car> updateCar(@PathVariable int index, @RequestBody Car newCar) {
+//		
+//		// replace the WHOLE object in a specific INDEX
+//		this.service.updateCar(index, newCar);
+//		
+//		return new ResponseEntity<Car>(newCar, HttpStatus.ACCEPTED);
+//		
+//		// OR Set each individual variable: 
+////		Car oldCar = this.vehicles.get(index); 
+////		oldCar.setMake(newCar.getMake());
+////		oldCar.setModel(newCar.getModel());
+////		oldCar.setEngine(newCar.getEngine());
+////		oldCar.setSpoiler(newCar.isSpoiler());
+//	}
 	
 	// DELETE - BY INDEX
 	@DeleteMapping("/removeCar/{index}")
-	public ResponseEntity removeCar(@PathVariable int index) {
+	public ResponseEntity removeCar(@PathVariable Long index) {
 		this.service.removeCar(index);
 		return new ResponseEntity(HttpStatus.NO_CONTENT);		
 	}
